4b03d3f2624b20a8c946e95a1f457fea95fcfe7d
GavinAllman <159083266+GavinAllman@users.noreply.github.com>
2025-11-15 14:37:19 -0500

    Created my homepage and first blog post. Used schemas and meta tags to see what the website would look like in the search engines. I do not have an icon yet so I expect it to have an error but is in the plans to fix!

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('cta-btn').addEventListener('click', function() {
+    alert('Fill in your contact details and let\'s connect about SEO!');
+});
+
+// Optional: Add more JS for interactivity as you learn
